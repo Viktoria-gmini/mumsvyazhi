46c5846686b6afe3a1b91773ebe8f42cb0c60e1e
Victoria <2905vikaef@gmail.com>
2023-10-12 17:48:16 +0300

    changed port

--- backend_system/src/main/java/com/knitwearteam/backend_system/BackendSystemApplication.java
+++ backend_system/src/main/java/com/knitwearteam/backend_system/BackendSystemApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSystemApplication.class, args);
 	}
-
 }
