458c8aeb1b9be138387abfe88efbf6d2b19adbf7
Victoria <2905vikaef@gmail.com>
2023-10-14 07:54:19 +0300

    allowed requests from any frontend url(attempt 4)

--- backend_system/src/main/java/com/knitwearteam/backend_system/controller/ProductController.java
+++ backend_system/src/main/java/com/knitwearteam/backend_system/controller/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "https://mumsik.onrender.com")
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/products")
 public class ProductController {
