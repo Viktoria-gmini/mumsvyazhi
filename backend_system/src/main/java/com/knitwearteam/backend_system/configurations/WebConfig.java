6d030ffd9424cbcead56751cef39c2c573fcded9
Victoria <2905vikaef@gmail.com>
2023-10-16 14:57:23 +0300

    allowed requests from any frontend url(attempt 6)

--- backend_system/src/main/java/com/knitwearteam/backend_system/configurations/WebConfig.java
+++ backend_system/src/main/java/com/knitwearteam/backend_system/configurations/WebConfig.java
@@ -1,17 +1,19 @@
 package com.knitwearteam.backend_system.configurations;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET, POST, PUT, DELETE, UPDATE")
-                .allowedOrigins("http://localhost:8080, https://mumsik.onrender.com")
-                .exposedHeaders("X-Get-Header")
-                .maxAge(3600);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000, http://mumsik-front.onreader.com");
+            }
+        };
     }
 }
