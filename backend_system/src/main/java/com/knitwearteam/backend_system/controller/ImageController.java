f98d9569d19d5df4f994e55e9a4491a359c14e29
Victoria <2905vikaef@gmail.com>
2023-10-17 23:56:48 +0300

    allowed requests to images from frontend url

--- backend_system/src/main/java/com/knitwearteam/backend_system/controller/ImageController.java
+++ backend_system/src/main/java/com/knitwearteam/backend_system/controller/ImageController.java
@@ -15,7 +15,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/images")
